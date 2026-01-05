91d38d3b14c678dfb9696d98e846f7e5d0bfd1a3
Brian Njogu <briannjogu85@gmail.com>
2026-01-05 16:26:22 +0300

    Add Schema facade import for table existence check

--- app/Http/Controllers/Finance/BalanceBroughtForwardController.php
+++ app/Http/Controllers/Finance/BalanceBroughtForwardController.php
@@ -13,6 +13,7 @@ use App\Services\InvoiceService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
 
