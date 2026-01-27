8dc7631b6d663d9302562e2f613384d75d379669
Brian Njogu <briannjogu85@gmail.com>
2026-01-27 08:58:32 +0300

    increase memory

--- public/index.php
+++ public/index.php
@@ -5,6 +5,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Avoid "Allowed memory size exhausted" in production (increase if you still hit limits)
+if (ini_get('memory_limit') !== '-1') {
+    $limit = (int) ini_get('memory_limit');
+    if ($limit > 0 && $limit < 256) {
+        @ini_set('memory_limit', '256M');
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
