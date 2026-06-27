ab1cc4451429560f3528c01238e4607d32e9a949
Brian Njogu <71688363+Njogu-Brian@users.noreply.github.com>
2026-06-27 23:32:44 +0300

    commit

--- resources/views/online_admissions/public_form.blade.php
+++ resources/views/online_admissions/public_form.blade.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     @php $hasVite = file_exists(public_path('build/manifest.json')); @endphp
     @if($hasVite)
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @elseif(file_exists(public_path('css/app.css')))
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @endif
